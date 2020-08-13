7e480cdfeb5b3d3fc8dec2f80e83baa6717cb09d
davemers0160 <davemers@iu.edu>
2020-08-13 07:22:54 -0400

    Update run_network_performance.h

--- common/include/run_network_performance.h
+++ common/include/run_network_performance.h
@@ -121,11 +121,11 @@ dlib::matrix<double, 1, 6> run_net_performace(std::ofstream &data_log_stream,
 #endif
 
         //save results to an image
-        //std::string image_save_name = image_save_location + "test_img_" + version + num2str(idx, "_%05d.png");
-        //save_png(rgb_img, image_save_name);
+        std::string image_save_name = image_save_location + "test_img_" + version + num2str(idx, "_%05d.png");
+        save_png(rgb_img, image_save_name);
 
         test_results += tr;
-        dlib::sleep(100);
+        dlib::sleep(25);
         //std::cin.ignore();
 
     }
